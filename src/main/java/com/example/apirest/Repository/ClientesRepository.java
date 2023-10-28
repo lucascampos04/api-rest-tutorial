a5748d47d30a2987f6a437cc4146338b0c75cc86
lucascampos04 <camposdlucasoli@gmail.com>
2023-10-28 11:33:08 -0300

    Consultas

---
+++ src/main/java/com/example/apirest/Repository/ClientesRepository.java
@@ -0,0 +1,9 @@
+package com.example.apirest.Repository;
+
+import com.example.apirest.model.Clientes;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClientesRepository extends JpaRepository<Clientes, Long> {
+}
